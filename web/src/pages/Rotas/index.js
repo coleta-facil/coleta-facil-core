edfce487257561d4826254edc00750f5cc985560
lucas-santosP <lucaspolicarpo.2018@gmail.com>
2021-05-22 19:48:33 -0300

    melhorando pagina de rotas

--- web/src/pages/Rotas/index.js
+++ web/src/pages/Rotas/index.js
@@ -1,97 +1,115 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
-import logo from "../../assets/icons/logo.svg";
-import "../../assets/styles/global.scss";
+import { nanoid } from "nanoid";
 import "./style.scss";
-import "../Welcome/style.scss";
-import "../Login/style.scss";
 import PageWrapper from "../../components/PageWrapper";
 import Header from "../../components/Header";
 import BaseButton from "../../components/BaseButton";
+import logo from "../../assets/icons/logo.svg";
+
+const emptyFormDevice = { name: "", code: "" };
 
 const Rotas = () => {
   const history = useHistory();
-
-  const [form, setForm] = useState({ deviceName: "", rota: "", code: "" });
+  const [devices, setDevices] = useState([]);
+  const [formDevice, setFormDevice] = useState(emptyFormDevice);
+  const [updateCode, setUpdateCode] = useState(false);
 
   function handleInputChange(event) {
     const { value, name } = event.target;
-    setForm((prev) => ({ ...prev, [name]: value }));
+    setFormDevice((prev) => ({ ...prev, [name]: value }));
+  }
+
+  async function fetchDevices() {
+    //const response = await  getDevices(); //firebase
+    //setDevices([...response.data]);
   }
 
   async function submitForm(event) {
     event.preventDefault();
-    var deviceName = document.getElementById("deviceName").value;
-    var codigo = document.getElementById("code").value;
-
-    //const regexPlaca = /^[a-zA-Z]{3}[0-9]{4}$/;
-
-    if (!deviceName) alert("Dados inválidos!");
-    else {
-      alert(codigo);
+    try {
+      if (!formDevice.name) throw new Error("Dados inválidos!");
+      formDevice.code = randomCode;
+      // await addDevice(formDevice) //firebase
+      setDevices((prev) => [...prev, { ...formDevice }]);
+      setFormDevice(emptyFormDevice);
+      setUpdateCode(true);
+    } catch (error) {
+      alert(error.message);
     }
   }
 
-  const randomCode = (len) => {
-    let code = "";
-    do {
-      code += Math.random().toString(36).substr(2).toUpperCase();
-    } while (code.length < len);
-    return code;
-  };
+  const randomCode = React.useMemo(() => {
+    if (updateCode) setUpdateCode(false);
+    return nanoid(4);
+  }, [updateCode]);
+
+  React.useEffect(() => {
+    fetchDevices();
+  }, []);
 
   return (
     <PageWrapper footerImage>
       <div className="rotas-page page-content">
         <Header>
           <img src={logo} className="logo" alt="logo" />
-          <BaseButton onClick={() => history.push("/cadastro")}>
-            Cadastrar usuário
-          </BaseButton>
+          <BaseButton onClick={() => history.push("/cadastro")}>Cadastrar usuário</BaseButton>
           <BaseButton type="submit" variant="logoff">
             Sair
           </BaseButton>
         </Header>
 
-        <div className="form-register-routes">
-          <h2>Cadastro de dispositivo</h2>
-          <form onSubmit={submitForm} className="form-group">
-            <label>Nome do dispositivo(apelido)</label>
-            <input
-              type="text"
-              name="placa"
-              id="placa"
-              placeholder="Nome do dispositivo"
-              value={form.deviceName}
-              onChange={handleInputChange}
-            />
+        <main>
+          <div className="aside-form-wrapper">
+            <form onSubmit={submitForm}>
+              <label>Nome do dispositivo (apelido)</label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Nome"
+                autoComplete="off"
+                value={formDevice.name}
+                onChange={handleInputChange}
+              />
 
-            <label>Rota</label>
-            <input
-              type="text"
-              name="rota"
-              id="rota"
-              placeholder="Nome da rota"
-              value={form.rota}
-              onChange={handleInputChange}
-            />
+              <label>ID do dispositivo (não pode ser editado)</label>
+              <input name="code" id="code" value={randomCode} readOnly />
+              <p>
+                ID únido usado na identificação do dispositivo <br />
+              </p>
 
-            <label>ID do dispositivo</label>
-            <input
-              name="code"
-              id="code"
-              value={randomCode(4)}
-              onChange={handleInputChange}
-              readOnly
-            />
-            <p>ID únido usado na identificação do dispositivo</p>
-            <p>(Não pode ser editado)</p>
+              <div className="btn-submit">
+                <BaseButton type={"submit"}>Cadastrar</BaseButton>
+              </div>
+            </form>
+          </div>
 
-            <div className="btn-submit">
-              <BaseButton type={"submit"}>Cadastrar</BaseButton>
-            </div>
-          </form>
-        </div>
+          <div className="aside-table-wrapper">
+            <h2 className="title">Dispositivos cadastrados</h2>
+            <table>
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>ID</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {devices.map((device) => (
+                  <tr key={device.code}>
+                    <td>{device.name}</td>
+                    <td>{device.code}</td>
+                  </tr>
+                ))}
+                {!devices.length && (
+                  <tr>
+                    <td colSpan="2">Nenhum dispositivo encontrado</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </main>
       </div>
     </PageWrapper>
   );
